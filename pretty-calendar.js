54f53495903d25b0dec0b9a4f840cb9d8ae840ea
Neel Somani <neeljaysomani@gmail.com>
2014-04-30 21:29:25 -0700

    Added new updates to JS file
    - added end times for events
    - fixed namespace issues
    - more accurate formatting when events are side-by-side

--- pretty-calendar.js
+++ pretty-calendar.js
@@ -1,31 +1,31 @@
-var weekView = true;
-var transition = false;
+PrettyCalendar.prototype.weekView = true;
+PrettyCalendar.prototype.transition = false;
 PrettyCalendar.UNDEFINED_TIME = -2;
 PrettyCalendar.prototype.wrappingDiv;
 
-function PrettyCalendar(events, divToPut, navigation, customLabels){
-	if(typeof navigation == 'undefined') navigation = false;
-	if(typeof customLabels == 'undefined') {
-		var weekday=new Array(7);
-		weekday[0]="Sunday";
-		weekday[1]="Monday";
-		weekday[2]="Tuesday";
-		weekday[3]="Wednesday";
-		weekday[4]="Thursday";
-		weekday[5]="Friday";
-		weekday[6]="Saturday";
-		customLabels = weekday;
-	}
-	this.wrappingDiv = divToPut;
-	this.genCalendar(customLabels);
-	if (navigation){
-		PrettyCalendar.addNavigation();
-	}
-	initTransitions();
-	PrettyCalendar.commitEvents(events);
+function PrettyCalendar(events, divToPut, navigation, customLabels) {
+    if (typeof navigation == 'undefined') navigation = false;
+    if (typeof customLabels == 'undefined') {
+        var weekday = new Array(7);
+        weekday[0] = "Sunday";
+        weekday[1] = "Monday";
+        weekday[2] = "Tuesday";
+        weekday[3] = "Wednesday";
+        weekday[4] = "Thursday";
+        weekday[5] = "Friday";
+        weekday[6] = "Saturday";
+        customLabels = weekday;
+    }
+    this.wrappingDiv = divToPut;
+    this.genCalendar(customLabels);
+    if (navigation) {
+        PrettyCalendar.addNavigation();
+    }
+    this.initTransitions();
+    PrettyCalendar.commitEvents(events);
 }
 
-PrettyCalendar.arrangeInDays = function(events) {
+PrettyCalendar.arrangeInDays = function (events) {
     var eventsToday = [];
     for (var i = 0; i < 7; i++) eventsToday[i] = [];
     for (var i = 0; i < events.length; i++) {
@@ -58,16 +58,19 @@ PrettyCalendar.arrangeInDays = function(events) {
         eventsToday[dayToArrange][tempIndex][0] = events[i][1];
         eventsToday[dayToArrange][tempIndex][1] = events[i][2];
         eventsToday[dayToArrange][tempIndex][2] = events[i][3];
+        if (events[i].length > 4) {
+            eventsToday[dayToArrange][tempIndex][3] = events[i][4];
+        }
     }
     return eventsToday;
 }
 
-PrettyCalendar.prototype.genCalendar = function(customLabels) {
-    $("#"+this.wrappingDiv).css("font-family","Tahoma,Arial,sans-serif");
-    $("#"+this.wrappingDiv).css("overflow-x", "hidden");
-    $("#"+this.wrappingDiv).css("overflow-y", "hidden");
-    $("#"+this.wrappingDiv).css("min-width", "760px");
-    $("#"+this.wrappingDiv).css("min-height", "500px");
+PrettyCalendar.prototype.genCalendar = function (customLabels) {
+    $("#" + this.wrappingDiv).css("font-family", "Tahoma,Arial,sans-serif");
+    $("#" + this.wrappingDiv).css("overflow-x", "hidden");
+    $("#" + this.wrappingDiv).css("overflow-y", "hidden");
+    $("#" + this.wrappingDiv).css("min-width", "760px");
+    $("#" + this.wrappingDiv).css("min-height", "500px");
     var wrapperDiv = document.createElement("div");
     $(wrapperDiv).attr("id", "wrapper");
     var calendarDiv = document.createElement("div");
@@ -91,7 +94,7 @@ PrettyCalendar.prototype.genCalendar = function(customLabels) {
         var dayLabel = document.createElement("div");
         $(dayLabel).attr("class", "dayName sep");
         var dayLabelText = "Sunday";
-		dayLabelText = customLabels[i];
+        dayLabelText = customLabels[i];
         $(dayLabel).text(dayLabelText);
         dayDiv.appendChild(dayLabel);
         for (var j = 0; j < 23; j++) {
@@ -106,9 +109,9 @@ PrettyCalendar.prototype.genCalendar = function(customLabels) {
     document.getElementById(this.wrappingDiv).appendChild(wrapperDiv);
 }
 
-PrettyCalendar.timeToHours = function(formatted) {
+PrettyCalendar.timeToHours = function (formatted) {
     var timeHours = 0;
-    var timeWithLabel = formatted[0];
+    var timeWithLabel = formatted;
     if (timeWithLabel.replace("pm", "") != timeWithLabel) timeHours += 12;
     var twoPieces = timeWithLabel.split(":");
     if (twoPieces[0] == "12") timeHours -= 12;
@@ -117,36 +120,47 @@ PrettyCalendar.timeToHours = function(formatted) {
     return timeHours;
 }
 
-PrettyCalendar.populateEvents = function(eventsToday) {
+PrettyCalendar.hoursToPercent = function (hours) {
+    return (100 * hours / 24) + 4.16;
+}
+
+PrettyCalendar.populateEvents = function (eventsToday) {
     var counterTemp = 0;
     for (var j = 0; j < 7; j++) {
         var lastTime = PrettyCalendar.UNDEFINED_TIME;
         var numToCompress = 1;
         if (eventsToday[j].length != 0) {
-            eventsToday[j] = eventsToday[j].sort(function(a, b) {
-                return PrettyCalendar.timeToHours(a) - PrettyCalendar.timeToHours(b);
+            eventsToday[j] = eventsToday[j].sort(function (a, b) {
+                return PrettyCalendar.timeToHours(a[0]) - PrettyCalendar.timeToHours(b[0]);
             });
         }
         for (var i = 0; i < eventsToday[j].length; i++) {
             counterTemp++;
-            var timeHours = PrettyCalendar.timeToHours(eventsToday[j][i]);
-            if (timeHours - lastTime < 1.2) {
+            var timeHours = PrettyCalendar.timeToHours(eventsToday[j][i][0]);
+            var percentTemp = PrettyCalendar.hoursToPercent(timeHours);
+            var lastPercentTemp = PrettyCalendar.hoursToPercent(lastTime);
+            if ($("#calendar").height() * lastPercentTemp / 100 + $("#event" + (counterTemp - 1)).innerHeight() > $("#calendar").height() * percentTemp / 100) {
                 numToCompress++;
             } else {
                 numToCompress = 1;
             }
-            lastTime = timeHours;
             var formatWidth = 100 / numToCompress;
+            lastTime = timeHours;
             var eventTempDiv = document.createElement("div");
             $(eventTempDiv).attr("class", "event");
             $(eventTempDiv).attr("id", "event" + (counterTemp));
-            var percentTemp = (100 * timeHours / 24) + 4.16;
-            $(eventTempDiv).attr("style", "top:" + percentTemp + "%;width:" + formatWidth + "%;background-color:" + eventsToday[j][i][2] + ";left:" + (100 - formatWidth) + "%;");
+            var heightSet = "height:auto;";
+            if (eventsToday[j][i].length > 3) {
+                heightSet = "height:" + (PrettyCalendar.hoursToPercent(PrettyCalendar.timeToHours(eventsToday[j][i][3])) - percentTemp) + "%;";
+            }
+            $(eventTempDiv).attr("style", "top:" + percentTemp + "%;width:" + formatWidth + "%;background-color:" + eventsToday[j][i][2] + ";left:" + (100 - formatWidth) + "%;" + heightSet);
             if (formatWidth != 100) {
                 for (var x = 0; x < numToCompress - 1; x++) {
                     $("#event" + (counterTemp - (x + 1))).css("width", formatWidth + "%");
                     $("#event" + (counterTemp - (x + 1))).css("left", (100 - formatWidth * (x + 2)) + "%");
+                    $("#event" + (counterTemp - (x + 1))).attr("title", $("#event" + (counterTemp - (x + 1))).text());
                 }
+                $(eventTempDiv).attr("title", eventsToday[j][i][1]);
             }
             $(eventTempDiv).text(eventsToday[j][i][1]);
             $("#day" + (j + 1)).append(eventTempDiv);
@@ -154,58 +168,59 @@ PrettyCalendar.populateEvents = function(eventsToday) {
     }
 }
 
-function initTransitions() {
-    $(".day").click(function() {
-        if (!transition) {
-            if (weekView) {
-                transition = true;
-                weekView = false;
+PrettyCalendar.prototype.initTransitions = function () {
+    var tempCal = this;
+    $(".day").click(function () {
+        if (!tempCal.transition) {
+            if (tempCal.weekView) {
+                tempCal.transition = true;
+                tempCal.weekView = false;
                 $("#day7").not(this).css("width", "13%");
                 $("#day1").not(this).css("width", "13%");
                 $(".day").not(this).css("min-width", "0px");
                 $(".day").not(this).animate({
                     width: "0%",
-                }, 1000, function() {
+                }, 1000, function () {
                     $(".day").not(this).css("display", "none");
                 });
                 $(this).animate({
                     width: "100%",
-                }, 1000, function() {
+                }, 1000, function () {
                     $(this).css("display", "block");
-                    transition = false;
+                    tempCal.transition = false;
                 });
             } else {
-                transition = true;
+                tempCal.transition = true;
                 $(".day").css("display", "block");
-                weekView = true;
+                tempCal.weekView = true;
                 $(".day").not(this).animate({
                     width: "14.28%",
-                }, 1000, function() {
+                }, 1000, function () {
                     $(".day").css("min-width", "100px");
-                    transition = false;
+                    tempCal.transition = false;
                 });
                 $(this).animate({
                     width: "14.28%",
                 }, 970);
-                    
+
             }
         }
     });
 }
 
-PrettyCalendar.prototype.addFooter = function(footerContents) {
+PrettyCalendar.prototype.addFooter = function (footerContents) {
     var footer = document.createElement("footer");
     $(footer).html(footerContents);
     document.getElementById(this.wrappingDiv).appendChild(footer);
 }
 
-PrettyCalendar.addNavigation = function() {
-	var leftNavBtn = '<div class="directionNav" onclick="leftNav()"><span class="directionLabel">&lt;</span></div>';
-	var rightNavBtn = '<div class="directionNav" onclick="rightNav()" style="left:95%;"><span class="directionLabel">&gt;</span></div>';
-	$("#wrapper").append(leftNavBtn+rightNavBtn);
+PrettyCalendar.addNavigation = function () {
+    var leftNavBtn = '<div class="directionNav" onclick="leftNav()"><span class="directionLabel">&lt;</span></div>';
+    var rightNavBtn = '<div class="directionNav" onclick="rightNav()" style="left:95%;"><span class="directionLabel">&gt;</span></div>';
+    $("#wrapper").append(leftNavBtn + rightNavBtn);
 }
 
-PrettyCalendar.commitEvents = function(events){
-	events = PrettyCalendar.arrangeInDays(events);
-	PrettyCalendar.populateEvents(events);
+PrettyCalendar.commitEvents = function (events) {
+    events = PrettyCalendar.arrangeInDays(events);
+    PrettyCalendar.populateEvents(events);
 }
